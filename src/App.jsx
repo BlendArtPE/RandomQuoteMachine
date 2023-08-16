5212d1da5d35836123667acd8e950be1de78c8c7
Daniel <jesus.cs.7.2010@gmail.com>
2023-08-15 19:50:04 -0500

    Finished the logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {quotes} from "./data/quotes"
 
 function App() {
   const [count, setCount] = useState(0)
+  const [quote, setQuote] = useState([]);
+
+  const random = () => {
+    const randomIndex = Math.floor(Math.random() * quotes.length); // Genera un número entre 0 y 10
+    setCount(randomIndex)
+    setQuote(quotes[randomIndex])
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      
+      <div className="card" id="quote-box">
+        <h1 id="text">{count === 0 ? quotes[0].frase : quote.frase}</h1>
+        <h3 id="author">-{quote.autor}</h3>
+        <button onClick={random}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/data/quotes.js
@@ -0,0 +1,53 @@
+export const quotes = [
+    {
+      "id": 0,
+      "frase": "La vida es lo que pasa mientras estás ocupado haciendo otros planes.",
+      "autor": "John Lennon"
+    },
+    {
+      "id": 1,
+      "frase": "El secreto de la creatividad es saber cómo ocultar tus fuentes.",
+      "autor": "Albert Einstein"
+    },
+    {
+      "id": 2,
+      "frase": "Nuestra mayor gloria no está en nunca caer, sino en levantarnos cada vez que caemos.",
+      "autor": "Confucio"
+    },
+    {
+      "id": 3,
+      "frase": "No importa lo lento que vayas, siempre y cuando no te detengas.",
+      "autor": "Confucio"
+    },
+    {
+      "id": 4,
+      "frase": "La única forma de hacer un gran trabajo es amar lo que haces.",
+      "autor": "Steve Jobs"
+    },
+    {
+      "id": 5,
+      "frase": "La vida es realmente simple, pero insistimos en hacerla complicada.",
+      "autor": "Confucio"
+    },
+    {
+      "id": 6,
+      "frase": "La mejor venganza es el éxito masivo.",
+      "autor": "Frank Sinatra"
+    },
+    {
+      "id": 7,
+      "frase": "El tiempo que disfrutas malgastar no es tiempo malgastado.",
+      "autor": "John Lennon"
+    },
+    {
+      "id": 8,
+      "frase": "No cuentes los días, haz que los días cuenten.",
+      "autor": "Muhammad Ali"
+    },
+    {
+      "id": 9,
+      "frase": "Si no tienes críticos, probablemente no tienes éxito.",
+      "autor": "Malcolm X"
+    }
+  ]
+  
