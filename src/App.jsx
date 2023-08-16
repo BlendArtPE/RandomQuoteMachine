ff48db5c7457ec9eb43d0af83c081e28db201361
Daniel <jesus.cs.7.2010@gmail.com>
2023-08-15 20:29:37 -0500

    New Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,40 @@
-import { useState } from 'react'
-import './App.css'
-import {quotes} from "./data/quotes"
+import { useState } from "react";
+import "./App.css";
+import { quotes } from "./data/quotes";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   const [quote, setQuote] = useState([]);
 
   const random = () => {
     const randomIndex = Math.floor(Math.random() * quotes.length); // Genera un número entre 0 y 10
-    setCount(randomIndex)
-    setQuote(quotes[randomIndex])
-  }
+    setCount(randomIndex);
+    setQuote(quotes[randomIndex]);
+  };
 
   return (
     <>
-      
-      <div className="card" id="quote-box">
-        <h1 id="text">{count === 0 ? quotes[0].frase : quote.frase}</h1>
-        <h3 id="author">-{quote.autor}</h3>
-        <button onClick={random}>
-          count is {count}
-        </button>
+      <div
+        className="d-flex justify-content-center align-items-center vh-100"
+        id="quote-box"
+      >
+        <div className="card p-5" id="quote-box">
+          <h1 className="text" id="text">
+            {count === 0 ? quotes[0].frase : quote.frase}
+          </h1>
+
+          <div className="text-end ">
+            <h3 className="author" id="author">
+              -{count === 0 ? quotes[0].autor : quote.autor}
+            </h3>
+            <button className="btn btn-primary" onClick={random}>
+              New Quote
+            </button>
+          </div>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
